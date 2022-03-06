67af8bf2dd88a4ba0b868bd37fb27dfce38cf0fc
Vijayan Sarathy <viji@sarathy.io>
2022-03-06 10:13:36 -0500

    Added support to run the side-casr alongside an OTel Collector

--- cmd/main.go
+++ cmd/main.go
@@ -12,27 +12,25 @@ import (
 	"github.com/aws-samples/prometheus-for-ecs/pkg/aws"
 )
 
-var prometheusConfigFilePath string
-var scrapeConfigFilePath string
+var present bool
+var configFileDir, configReloadFrequency string
+var prometheusConfigFilePath, scrapeConfigFilePath string
 
 func main() {
 	log.Println("Prometheus configuration reloader started")
 	aws.InitializeAWSSession()
 
-	configFileDir, present := os.LookupEnv("CONFIG_FILE_DIR")
+	configFileDir, present = os.LookupEnv("CONFIG_FILE_DIR")
 	if !present {
 		configFileDir = "/etc/config/"
 	}
-	configReloadFrequency, present := os.LookupEnv("CONFIG_RELOAD_FREQUENCY")
+	configReloadFrequency, present = os.LookupEnv("CONFIG_RELOAD_FREQUENCY")
 	if !present {
 		configReloadFrequency = "30"
 	}
-	prometheusConfigFilePath = strings.Join([]string{configFileDir, "prometheus.yaml"}, "/")
-	scrapeConfigFilePath = strings.Join([]string{configFileDir, "ecs-services.json"}, "/")
 
 	loadPrometheusConfig()
-	loadScrapeConfig()
-	log.Println("Loaded initial configuration file")
+	initScrapeTargetConfig()
 
 	go func() {
 		reloadFrequency, _ := strconv.Atoi(configReloadFrequency)
@@ -65,28 +63,42 @@ func main() {
 }
 
 func loadPrometheusConfig() {
+	// When deployed with OTel Collector, Prometheus runs as a Receiver in the OTel Pipeline
+	// Hence, Prometheus configuration is part of the OTel Pipeline configuration which is loaded from SSM
+	deploymentModeParameter, present := os.LookupEnv("DEPLOYMENT_MODE")
+	if present && (deploymentModeParameter == "OTEL" || deploymentModeParameter == "otel") {
+		log.Println("Running in OTel mode")
+		return
+	}
+
 	prometheusConfigParameter, present := os.LookupEnv("PROMETHEUS_CONFIG_PARAMETER_NAME")
 	if !present {
-			prometheusConfigParameter = "ECS-Prometheus-Configuration"
+		prometheusConfigParameter = "ECS-Prometheus-Configuration"
 	}
 	prometheusConfig := aws.GetParameter(prometheusConfigParameter)
+
+	prometheusConfigFilePath = strings.Join([]string{configFileDir, "prometheus.yaml"}, "/")
 	err := ioutil.WriteFile(prometheusConfigFilePath, []byte(*prometheusConfig), 0644)
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("Loaded Prometheus configuration file")
+
 }
 
-func loadScrapeConfig() {
+func initScrapeTargetConfig() {
+	scrapeConfigFilePath = strings.Join([]string{configFileDir, "ecs-services.json"}, "/")
 	err := ioutil.WriteFile(scrapeConfigFilePath, []byte("[]"), 0644)
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("Created initial scrape target configuration file")
 }
 
 func reloadScrapeConfig() {
 	discoveryNamespacesParameter, present := os.LookupEnv("DISCOVERY_NAMESPACES_PARAMETER_NAME")
 	if !present {
-			discoveryNamespacesParameter = "ECS-ServiceDiscovery-Namespaces"
+		discoveryNamespacesParameter = "ECS-ServiceDiscovery-Namespaces"
 	}
 	namespaceList := aws.GetParameter(discoveryNamespacesParameter)
 	namespaces := strings.Split(*namespaceList, ",")
